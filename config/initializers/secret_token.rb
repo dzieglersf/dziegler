5a5a117ba207e534e8b4b227f0f87b5d07b19ebf
David Ziegler <dzieglersf@gmail.com>
2012-06-22 17:16:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dziegler::Application.config.secret_token = '7dfba5633959fe8547158e602fa220ab018e0c09f47871bc9ad3b4ea67bdd96ffa4a23f494c8dca45bc0b379c00571387cf363f82a2e427d3ba51dee45301c26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dziegler::Application.config.session_store :cookie_store, key: '_dziegler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dziegler::Application.config.session_store :active_record_store
