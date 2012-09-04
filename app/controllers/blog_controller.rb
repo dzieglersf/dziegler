946bfac130dfeb778bf26005b6e77a7694f0a8ca
David Ziegler <dzieglersf@gmail.com>
2012-09-04 11:50:25 -0700

    made blog singleton

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,14 +1,6 @@
 class BlogController < ApplicationController
   
   def index
-  	@blog = Blog.new
-  	post1 = @blog.new_post
-  	post1.title = "Amazing new idea"
-  	post1.body = "I just thought of this idea tonight!"
-  	post1.publish
-  	post2 = @blog.new_post( title: "Idea lost forever!" )
-  	post2.body = "I spilled Jamesons on the keyboard and I'll never get the idea back."
-  	post2.publish
   end
 
 end
