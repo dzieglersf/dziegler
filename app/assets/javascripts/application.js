955189a216827fabde583f2fc37be7f93af11357
David Ziegler <dzieglersf@gmail.com>
2012-09-15 09:24:30 -0700

    added dropdown, cleanedup stylesheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
+// require twitter/bootstrap
 //= require Raphael
 //= require_tree .
+
+$(document).ready(function(){
+	$('.dropdown-toggle').dropdown();  
+});
